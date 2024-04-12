1e0a049bc4f80f9672e8d713b8c4187c89152362
Steven <stevenrafaelie@gmail.com>
2024-04-12 10:09:28 +0700

    Base File

---
+++ src/main/java/com/srl/N2quizsrl/N2QuizSrlApplication.java
@@ -0,0 +1,13 @@
+package com.srl.N2quizsrl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N2QuizSrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N2QuizSrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srl/N2quizsrl/N2QuizSrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srl.N2quizsrl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N2QuizSrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
